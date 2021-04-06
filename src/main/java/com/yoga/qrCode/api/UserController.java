3b4708263305ca00e281ce69fd8368925a7a6d45
yogakani <yogakani32@gmail.com>
2021-04-06 17:28:50 +0530

    Added loggers

--- src/main/java/com/yoga/qrCode/api/UserController.java
+++ src/main/java/com/yoga/qrCode/api/UserController.java
@@ -5,6 +5,7 @@ import com.yoga.qrCode.model.response.Response;
 import com.yoga.qrCode.model.request.UserRequest;
 import com.yoga.qrCode.model.response.UserResponse;
 import com.yoga.qrCode.service.UserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +17,7 @@ import static com.yoga.qrCode.utils.CommonUtils.getHttpStatusCode;
 
 @RequestMapping(value = "/api/v1.0/user")
 @RestController
+@Slf4j
 public class UserController {
 
     @Autowired
@@ -23,14 +25,18 @@ public class UserController {
 
     @PostMapping(value = "/create")
     public ResponseEntity<Response> createUser(@RequestBody UserRequest userRequest, @RequestHeader("requestId") String requestId) {
+        log.info("RequestId : {} - Create User process starts..",requestId);
         userRequest.setRequestId(requestId);
         boolean status = userService.createNewUser(userRequest);
+        log.info("RequestId : {} - Create User process ends..",requestId);
         return new ResponseEntity<>(new Response().setStatus(status), getHttpStatusCode(status));
     }
 
     @GetMapping(value = "/get")
     public ResponseEntity<?> getUser(@RequestHeader("userId") String userId, @RequestHeader("requestId") String requestId) {
+        log.info("RequestId : {} - Get User process starts..",requestId);
         Optional<UserResponse> userResponseOp = userService.getUser(userId, requestId);
+        log.info("RequestId : {} - Get User process ends..",requestId);
         return userResponseOp.isPresent() ?
              new ResponseEntity<>(userResponseOp.get().setStatus(true), getHttpStatusCode(true)) :
              new ResponseEntity<>(new Response().setStatus(false), getHttpStatusCode(false));
